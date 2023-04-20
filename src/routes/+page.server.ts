25307525488a3fa1cbccb0557f06cb94d941a603
Julian Stephens <julian@julianstephens.net>
2023-04-19 23:17:38 -0400

    fix: filter duplicate results

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,11 +4,18 @@ import client from "$lib/server/client";
 
 export const actions = {
     default: async ({ request }) => {
-        const data = await request.formData();
-        const query = data.get("query")?.toString();
+        const formData = await request.formData();
+        const query = formData.get("query")?.toString();
 
         if (!query) return fail(400, { query, missing: true })
         const res = await client.graphql.get().withClassName("Snippet").withFields("episodeName episodeNo").withNearText({ concepts: [...query.split(" ")] }).do()
-        return { type: "success", data: res.data.Get.Snippet }
+        let data = res.data.Get.Snippet;
+        console.log(data)
+        data = [...new Map(data.map((item: any) =>
+            [item["episodeNo"], item])).values()]
+        console.log(data)
+
+
+        return { type: "success", data }
     }
 } satisfies Actions;
